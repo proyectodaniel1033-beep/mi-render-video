3b9ccfa2dfaf60f15ab430dee7e9145a74f11122
proyectodaniel1033-beep <proyectodaniel1033@gmail.com>
2026-07-26 23:20:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 @app.get("/download/{job_id}")
 def download_video(job_id: str):
     video_path = f"/app/videos/{job_id}.mp4"
+    
     if os.path.exists(video_path):
         return FileResponse(video_path, media_type="video/mp4", filename="video.mp4")
-    return {"error": "No encontrado"}
+    else:
+        return {"error": "El video aún no está listo o no existe"}
