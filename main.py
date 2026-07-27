dfb0f149eae5f267f21bdb153a0fe8b63a7f7873
proyectodaniel1033-beep <proyectodaniel1033@gmail.com>
2026-07-27 01:51:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/status/{identificacion}")
 async def check_status(identificacion: str):
-    # Asegúrate de que esta línea esté descomentada y apunte exactamente a donde se crea el archivo
+    # Sin el símbolo # al inicio para que la línea funcione:
     file_path = f"{identificacion}.mp4"
 
     if os.path.exists(file_path):
