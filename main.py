fad7b87fc1303a3c8ea555e1534e3b11c33136dd
proyectodaniel1033-beep <proyectodaniel1033@gmail.com>
2026-07-30 03:06:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def iniciar_transcodificacion(datos: TranscodeRequest, background_tasks: Backgro
 @app.get("/status/{task_id}")
 def obtener_estado(task_id: str):
     estado = estados_tareas.get(task_id, "no_encontrado")
-    return {"id": task_id, "estado": estado}
+    return {"id": task_id, "status": status}
